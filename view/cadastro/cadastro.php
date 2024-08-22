e1ca97cb0ea0783a08109c9cc0b37a909bfa6fdc
lisaniaSoares <lisasoares.if@gmail.com>
2024-08-21 23:56:30 -0300

    unificação dos cadastros

--- view/cadastro/cadastro.php
+++ view/cadastro/cadastro.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cadastre-se</title>
-        <link rel="stylesheet" href="vendedor.css">
+        <link rel="stylesheet" href="cadastro.css">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Bootstrap Icons CSS (opcional, para ícones) -->
@@ -17,7 +17,7 @@
                 <img src="header.png" alt="">
             </div>
 
-            <div style="margin-top: 2%; height: 5%" class="d-flex justify-content-center">
+            <div style="margin-top: 2%; height: 8%" class="d-flex justify-content-center align-items-center">
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="formDiv1" onchange="mostrarForm()">
                     <label class="form-check-label" for="inlineRadio1">Vendedor Autonômo</label>
@@ -25,6 +25,10 @@
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="formDiv2" onchange="mostrarForm()" >
                     <label class="form-check-label" for="inlineRadio2">Empresa</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="formDiv3" onchange="mostrarForm()" >
+                    <label class="form-check-label" for="inlineRadio3">Cliente</label>
                 </div>
 
             </div>
@@ -151,11 +155,75 @@
                                     </div>
 
                                     <div class="d-flex justify-content-between mb-5">
-                                        <button type="submit" class="btn btn-primary" style="background-color: #4ABFB2; border-color: #3BA99C; width: 30%" >Enviar</button>
-                                        <button type="button" class="btn btn-light" style="background-color: #C1ECE7; border-color: #C1ECE7;" >Voltar</button>
+                                        <button type="submit" class="btn btn-primary" id="enviar" >Enviar</button>
+                                        <button type="button" class="btn btn-light" id="voltar" >Voltar</button>
                                     </div>
                                 </form>
                         </div>
+
+                        <div id="formDiv3">
+                            <form>
+                              <div class="mb-3">
+                                  <label for="nome" class="form-label">Nome Completo</label>
+                                  <input type="text" class="form-control form-control-lg" id="nome-empresa" placeholder="">
+                              </div>
+      
+                              <div class="mb-3">
+                                  <label for="email" class="form-label">Email</label>
+                                  <input type="email" class="form-control form-control-lg" id="email" placeholder="">
+                              </div>
+                          
+                              <div class="row">
+                                  <div class="col-md-6 mb-3">
+                                      <label for="cpf" class="form-label">CPF</label>
+                                      <input type="text" class="form-control form-control-lg" id="cnpj" placeholder="">
+                                  </div>
+                                  <div class="col-md-6 mb-3">
+                                      <label for="datanascimento" class="form-label">Data de Nascimento</label>
+                                      <input type="date" class="form-control form-control-lg" id="data-nascimento" placeholder="">
+                                  </div>
+                              </div>
+      
+                              <div class="row">
+                                  <div class="col-md-6 mb-3">
+                                      <label for="genero" class="form-label">Genêro</label>
+                                      
+                                <select class="form-control form-control-lg" id="exampleFormControlSelect1">
+                                    <option>Masculino</option>
+                                    <option>Feminino</option>
+                                    <option>Prefiro não informar</option>
+                                </select>
+                                  </div>
+                                  <div class="col-md-6 mb-3">
+                                      <label for="celular" class="form-label">Celular</label>
+                                      <input type="text" class="form-control form-control-lg" id="celular" placeholder="">
+                                  </div>
+                              </div>
+      
+      
+                              <div class="row">
+                                  <div class="col-md-6 mb-3">
+                                      <label for="senha1" class="form-label">Senha</label>
+                                      <input type="password" class="form-control form-control-lg" id="senha" placeholder="Digite sua senha">
+                                  </div>
+                                  <div class="col-md-6 mb-3">
+                                      <label for="senha2" class="form-label">Confirmação de senha</label>
+                                      <input type="password" class="form-control form-control-lg" id="confirmacao-senha" placeholder="Confirme sua senha">
+                                  </div>
+                              </div>
+      
+                              <div class="mb-3 form-check">
+                                  <input type="checkbox" class="form-check-input" id="terms">
+                                  <label class="form-check-label" for="terms">Concordo com os termos</label>
+                              </div>
+      
+                              <div class="d-flex justify-content-between">
+                                  <button type="submit" class="btn btn-primary" id="enviar">Enviar</button>
+                                  <button type="button" class="btn btn-light" id="voltar">Voltar</button>
+                              </div>
+                          </form>
+                        </div>
+
                     </div>
                     </div>
                 </div>
@@ -171,6 +239,7 @@
                 // Esconder todos os formulários
                 document.getElementById("formDiv1").style.display = "none";
                 document.getElementById("formDiv2").style.display = "none";
+                document.getElementById("formDiv3").style.display = "none";
 
                 // Obter o valor do radio button selecionado
                 var radios = document.getElementsByName("inlineRadioOptions");
@@ -179,8 +248,11 @@
                      //o value deve ser o mesmo id da div a que corresponde
                     document.getElementById(radios[0].value).style.display = "block";
 
-                } else{
+                } else if(radios[1].checked){
                     document.getElementById(radios[1].value).style.display = "block";
+
+                } else{
+                    document.getElementById(radios[2].value).style.display = "block";
                 }
             }
 
