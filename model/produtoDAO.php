87580a14075d339c7dbbc13d1ce5ea44bc166aac
Goldner027 <goldner.027@gmail.com>
2024-08-25 21:40:59 -0300

    Create produtoDAO.php
    Criação do produtoDAO

---
+++ model/produtoDAO.php
@@ -0,0 +1,42 @@
+<?php
+
+require "conexaoBD.php";    
+static $conexao;
+
+function inserirProduto($vendedor, $status, $ano, $preco, $imagem, $descricao, $subcategoria, $condicao) {
+
+    $conexao = conectarBD();    
+
+    $tamanhoImg = $imagem["size"]; 
+    $arqAberto = fopen ( $imagem["tmp_name"], "r" );
+    $img = addslashes( fread ( $arqAberto , $tamanhoImg ) );
+
+    
+    // Montar SQL
+    $sql = "INSERT INTO Produto 
+        (user_Vendedor_idVendedor, statusProduto, anoProduto, precoProduto, imagemProduto, descricaoProduto, Subcategoria_idSubvategoria, condicaoProduto)
+        VALUES ('$vendedor', '$status', '$ano', '$preco', '$img', '$descricao', '$subcategoria', '$condicao') {
+)";
+
+    mysqli_query($conexao, $sql) or die ( mysqli_error($conexao) );     // Inserir no banco
+    
+    // Pega o código inserido
+    $id = mysqli_insert_id($conexao);  
+    return $id;
+}
+
+function atualizarProduto() {
+
+}
+
+function excluirProduto() {
+
+}
+
+function pesquisarProdutoPorNome() {
+
+}
+
+function getProduto() {
+
+}
