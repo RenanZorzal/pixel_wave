9adcdc634a9cc55aa7f8456944857b1dda7482ae
Alaerte <alaertetimm1401@gmail.com>
2024-09-06 16:57:19 -0300

    Update dados-bd.sql

--- dados-bd.sql
+++ dados-bd.sql
@@ -39,4 +39,4 @@ INSERT INTO comprador (nomeComprador, emailComprador, telefoneComprador, data_na
 ('Jãozin', 'jaobonitininho123456@gmail.com', '2211111111', '2000-01-01', '111.222.333-44', null, '123456789');
 
 INSERT INTO produto(Vendedor_idVendedor, nomeProduto, statusProduto, anoProduto, precoProduto, imagemProduto, descricaoProduto, Subcategoria_idSubcategoria, condicaoProduto, qtdEstoque) VALUES
-(1, 'Motorola', 1, 2020, '2691' , null, 'smartphone motorola', 5, 1, 1);
+(1, 'Motorola', 1, 2020, 2691.99 , null, 'smartphone motorola', 5, 1, 1);
