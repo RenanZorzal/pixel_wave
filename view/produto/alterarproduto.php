c821186ec6b8658d9e7a137a8cd2ce3b05d4ce9f
Renan Zorzal <renanzberger@gmail.com>
2024-09-23 11:30:06 -0300

    get de produto

--- view/produto/alterarproduto.php
+++ view/produto/alterarproduto.php
@@ -6,7 +6,6 @@
   <title>Pixel Wave</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Bootstrap Icons CSS (opcional, para ícones) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <link rel="stylesheet" href="produto.css">
   <link rel="stylesheet" href="../navbar/estilo.css">
@@ -21,6 +20,7 @@
 <?php
 require_once "../navbar/navbarVendEmp.php";
 require_once '../../model/produtoDAO.php';
+
 $resultado = pesquisarProdutoPorID(1);
 $registro = mysqli_fetch_assoc($resultado);
 $nome = $registro["nomeProduto"];
@@ -29,8 +29,7 @@ $ano = $registro["anoProduto"];
 $preco = $registro["precoProduto"];
 $descricaoProduto = $registro["descricaoProduto"];
 $condicao = $registro["condicaoProduto"];
-$estoque = $registro 
-
+$estoque = $registro["qtdEstoque"];
 ?>
 
 <!--Página-->
@@ -40,18 +39,18 @@ $estoque = $registro
     <div class="row mb-3">
       <div class="col-md-8">
         <label for="nome" class="form-label"><b>Nome</b></label>
-        <input type="text" class="form-control" id="nome" name="nome">
+        <input type="text" class="form-control" id="nome" name="nome" value="<?php echo $nome; ?>">
       </div>
     </div>
     <div class="row mb-3 mt-3">
       <div class="col-md-8">
         <label class="form-label"><b>Condição</b></label><br>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="condicao" id="novo" value="novo" required>
+          <input class="form-check-input" type="radio" name="condicao" id="novo" value="novo" <?php if ($condicao == 'nova') echo 'checked'; ?> required>
           <label class="form-check-label" for="novo">Novo</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="condicao" id="usado" value="usado">
+          <input class="form-check-input" type="radio" name="condicao" id="usado" value="usado" <?php if ($condicao == 'seminova') echo 'checked'; ?>>
           <label class="form-check-label" for="usado">Usado</label>
         </div>
       </div>
@@ -60,57 +59,57 @@ $estoque = $registro
     <div class="row mb-3">
       <div class="col-md-8">
         <label for="anoLancamento" class="form-label"><b>Ano de lançamento</b></label>
-        <input type="number" class="form-control" id="anoLancamento" name="anoLancamento">
+        <input type="number" class="form-control" id="anoLancamento" name="anoLancamento" value="<?php echo $ano; ?>">
       </div>
     </div>
 
     <div class="row mb-3">
       <div class="col-md-8">
         <label for="descricao" class="form-label"><b>Descrição da peça</b></label>
-        <input type="text" class="form-control" id="descricao" name="descricao">
+        <input type="text" class="form-control" id="descricao" name="descricao" value="<?php echo $descricaoProduto; ?>">
       </div>
     </div>
-    
-    <div class="row mb-3">
-  <div class="col-md-8">
-    <label for="categoria" class="form-label"><b>Categoria</b></label>
-    <select class="form-control" id="categoria" name="categoria" onchange="carregarSubcategorias(this.value)">
-      <option>Selecione uma categoria</option>
-      <?php
-        require "../../model/categoriaDAO.php";
-        $options = carregarComboCategoria($categoria);              
-        echo $options;
-      ?>
-    </select>
-  </div>
-</div>
-<div class="row mb-3">
-  <div class="col-md-8">
-    <label for="subcategoria" class="form-label"><b>Sub-Categoria</b></label>
-    <select class="form-control" id="subcategoria" name="subcategoria">
-      <!-- Subcategorias serão carregadas aqui via AJAX -->
-    </select>
-  </div>
-</div>
 
-<script>
-  function carregarSubcategorias(categoriaId) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "../../control/produto/carregar_subcategorias.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        document.getElementById("subcategoria").innerHTML = xhr.responseText;
+    <div class="row mb-3">
+      <div class="col-md-8">
+        <label for="categoria" class="form-label"><b>Categoria</b></label>
+        <select class="form-control" id="categoria" name="categoria" onchange="carregarSubcategorias(this.value)">
+          <option>Selecione uma categoria</option>
+          <?php
+            require "../../model/categoriaDAO.php";
+            $options = carregarComboCategoria($categoria);
+            echo $options;
+          ?>
+        </select>
+      </div>
+    </div>
+    <div class="row mb-3">
+      <div class="col-md-8">
+        <label for="subcategoria" class="form-label"><b>Sub-Categoria</b></label>
+        <select class="form-control" id="subcategoria" name="subcategoria">
+          <!-- Subcategorias serão carregadas aqui via AJAX -->
+        </select>
+      </div>
+    </div>
+
+    <script>
+      function carregarSubcategorias(categoriaId) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "../../control/produto/carregar_subcategorias.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("subcategoria").innerHTML = xhr.responseText;
+          }
+        };
+        xhr.send("categoria=" + categoriaId);
       }
-    };
-    xhr.send("categoria=" + categoriaId);
-  }
-</script>
+    </script>
 
     <div class="row mb-3">
       <div class="col-md-8">
         <label for="preco" class="form-label"><b>Preço</b></label>
-        <input type="number" class="form-control" id="preco" name="preco">
+        <input type="number" class="form-control" id="preco" name="preco" value="<?php echo $preco; ?>">
       </div>
     </div>
 
@@ -118,18 +117,17 @@ $estoque = $registro
       <div class="col-md-8">
         <label class="form-label"><b>Status</b></label><br>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="status" id="sestoque" value="Sem estoque">
+          <input class="form-check-input" type="radio" name="status" id="sestoque" value="Sem estoque" <?php if ($status == 'Sem estoque') echo 'checked'; ?>>
           <label class="form-check-label" for="sestoque">Sem estoque</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="status" id="ind" value="quantidade">
-          <label class="form-check-label" for="ind">Em estoque</label>
+          <input class="form-check-input" type="radio" name="status" id="ind" value="disponível" <?php if ($status == 'disponível') echo 'checked'; ?>>
+          <label class="form-check-label" for="ind">Disponível</label>
         </div>
         
-        <!-- Campo para inserir a quantidade -->
-        <div id="quantidade-campo" class="mt-3">
+        <div id="quantidade-campo" class="mt-3 <?php echo ($status == 'Sem estoque') ? 'hidden' : ''; ?>">
           <label for="quantidade" class="form-label"><b>Quantidade</b></label>
-          <input type="number" id="quantidade" class="form-control" name="quantidade" placeholder="Digite a quantidade">
+          <input type="number" id="quantidade" class="form-control" name="quantidade" placeholder="Digite a quantidade" value="<?php echo $estoque; ?>">
         </div>
       </div>
     </div>
@@ -143,7 +141,7 @@ $estoque = $registro
 
     <div class="row mt-3">
       <div class="col-md-8">
-        <button type="submit" class="btn btn-primary w-100" style="background-color: #502779; border-color:#502779 ">Avançar</button>
+        <button type="submit" class="btn btn-primary w-100" style="background-color: #502779; border-color:#502779">Avançar</button>
       </div>
     </div>
   </form>
@@ -156,8 +154,6 @@ $estoque = $registro
   }
   ?> 
 </div>
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
