a707e8dd7c75a6445ac8371293a262fa509b629a
lisaniaSoares <lisasoares.if@gmail.com>
2024-10-11 21:14:19 +0100

    Logout Finalizado

--- view/login/login.php
+++ view/login/login.php
@@ -8,10 +8,19 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Bootstrap Icons CSS (opcional, para ícones) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="loginStyle.css">
+    <link rel="stylesheet" href="styleLogin.css">
     <link rel="stylesheet" href="../footer/footer-style.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 
 </head>
 <body>
@@ -31,7 +40,9 @@
 
             <div class="formulario-box">
 
-                <form method="post" name="formLogin" action="../../control/login/logar.php">
+                <form method="post" name="formLogin" class="itens-formulario" action="../../control/login/logar.php" enctype="multipart/form-data">
+
+                    <img src="img/login.png" alt="Login" class="login">
 
                     <div style="margin-top: 2%; height: 8%" class="radios">
 
@@ -49,8 +60,6 @@
                         </div>
 
                     </div>
-
-                    <img src="img/login.png" alt="Login" class="login">
 
                     <div class="input-group flex-nowrap input-login">
                         <span class="input-group-text" id="addon-wrapping">@</span>
@@ -83,22 +92,60 @@
 
     </main>
     
+    <?php
+        // Verifica se há uma mensagem de erro passada via GET
+        $mensagemErro = '';
 
+        if(isset($_GET["msgErro"])){
+            $mensagemErro = $_GET["msgErro"];
+            //echo "<FONT color=red>$mensagemErro</FONT>";
+        }
+    ?>
 
+    <!-- Modal -->
+    <div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" style="color: #FF6565; font-weight: bold">Feedback de Login</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <?php if (!empty($mensagemErro)) {  // Verifica se tem mensagem de ERRO
+                        echo "<FONT color=#FF6565>$mensagemErro</FONT>";
+                    } ?>
+                </div>
+                <!--<div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>-->
+            </div>
+        </div>
+    </div>
 
+    <?php 
 
+       if (!empty($mensagemErro)) {
 
+            // Exibe o modal se houver uma mensagem de erro
+            echo '<script type="text/javascript">
+                        window.onload = function() {
+                            console.log("modal");
+                            var errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
+                            errorModal.show();
+                        }
+                    </script>';
 
+            // Redireciona para a mesma página sem o parâmetro `msg` após mostrar o modal
+            echo '<script type="text/javascript">
+                    window.onload = function() {
+                        var errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
+                        errorModal.show();
+                        history.replaceState(null, "", window.location.href.split("?")[0]);
+                    }
+                </script>';
+        }
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    ?>
 
     <script src="app.js"></script>
 
