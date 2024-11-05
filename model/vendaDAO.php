78f5371e3ac3dd3d641a956cc83c9bc9d5c815e6
Alaerte <alaertetimm1401@gmail.com>
2024-11-05 09:08:37 -0300

    Add files via upload

---
+++ model/vendaDAO.php
@@ -0,0 +1,35 @@
+<?php
+
+    require_once "conexaoBD.php";    
+    static $conexao;
+
+    function inserirVenda($idCliente ) {
+        $conexao = conectarBD();  
+
+        $sql = "INSERT INTO VendaCompra (dataHora, nota_fiscal, valorTotal, Comprador_idComprador, StatusCompra_idStatusCompra) "
+                . "VALUES ( NOW(), null, 0.0, $idCliente, 'Andamento') ";
+        
+        mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+        $id = mysqli_insert_id($conexao);
+        return $id;
+    }
+
+    
+    function inserirItemVenda($idProduto, $idVenda, $qtde){
+        $conexao = conectarBD();   
+
+        $sql = "INSERT INTO ItensVendaCompra (VendaCompra_idVendaCompra, Produto_idProduto, qtdeItens) "
+                . "VALUES ( $idVenda, $idProduto, $qtde)";
+        
+        mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+    }
+
+    
+    function alterarTotal ($idVenda, $total) {
+        $conexao = conectarBD();  
+        
+        $sql = "UPDATE VendaCompra SET valorTotal = $total WHERE idVendaCompra = $idVenda";
+        mysqli_query($conexao, $sql) or die (mysqli_error($conexao));        
+    }
+?>
+
