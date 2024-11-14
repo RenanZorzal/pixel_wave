027b3a595f7293c92fe107c7f092f60e7edad250
Alaerte <alaertetimm1401@gmail.com>
2024-11-14 14:28:08 -0300

    Update vendaDAO.php

--- model/vendaDAO.php
+++ model/vendaDAO.php
@@ -31,5 +31,42 @@
         $sql = "UPDATE VendaCompra SET valorTotal = $total WHERE idVendaCompra = $idVenda";
         mysqli_query($conexao, $sql) or die (mysqli_error($conexao));        
     }
+
+    function atualizarEstoque($idVenda) {
+        $conexao = conectarBD();
+    
+        // Consulta para buscar os itens da venda
+        $sql = "SELECT iv.Produto_idProduto, iv.qtdeItens
+                FROM ItensVendaCompra AS iv
+                WHERE iv.VendaCompra_idVendaCompra = $idVenda";
+    
+        $resultado = mysqli_query($conexao, $sql);
+    
+        if (!$resultado) {
+            die("Erro ao buscar itens da venda: " . mysqli_error($conexao));
+        }
+    
+        // Atualiza o estoque para cada produto
+        while ($item = mysqli_fetch_assoc($resultado)) {
+            $idProduto = $item['Produto_idProduto'];
+            $quantidadeComprada = $item['qtdeItens'];
+    
+            $sqlAtualizar = "UPDATE Produto 
+                             SET qtdEstoque = qtdEstoque - $quantidadeComprada 
+                             WHERE idProduto = $idProduto";
+    
+            if (!mysqli_query($conexao, $sqlAtualizar)) {
+                die("Erro ao atualizar estoque do produto $idProduto: " . mysqli_error($conexao));
+            }
+        }
+    
+        mysqli_close($conexao);
+        return true;
+    }
+
+
+
+
+
 ?>
 
