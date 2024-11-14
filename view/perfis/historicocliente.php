0ddbcb641679362f3e82732a88159e6222c8405e
Alaerte <alaertetimm1401@gmail.com>
2024-11-14 08:32:58 -0300

    Update historicocliente.php

--- view/perfis/historicocliente.php
+++ view/perfis/historicocliente.php
@@ -1,13 +1,5 @@
 <?php
 session_start();
-require_once '../../model/clienteDAO.php';
-require_once '../../model/vendaDAO.php'; // Assumindo que você tem um DAO para vendas também
-
-$idComprador = $_SESSION["idSessao"]; // Obtém o ID do comprador logado
-
-// Função para buscar o histórico de compras
-$resultado = buscarHistorico($idComprador);
-
 ?>
 
 <!DOCTYPE html>
@@ -17,9 +9,40 @@ $resultado = buscarHistorico($idComprador);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Histórico de Compras</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <!-- Bootstrap Icons CSS (opcional, para ícones) -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="../navbar/estilo.css">
+  <link rel="stylesheet" href="../footer/footer-style.css">  
 </head>
 <body>
 
+
+<?php
+
+require_once "../../control/login/validarSessao.php";
+
+$tipoSessao = validarSessao(false, false, true); // Valida a sessão e retorna o tipo
+
+if ($tipoSessao === 'cliente') { // Verifica se é CLIENTE
+    require_once "../navbar/navbarCliente.php";
+    require_once '../../model/clienteDAO.php';
+
+} elseif ($tipoSessao === 'vendedor' || $tipoSessao === 'empresa') { // Verifica se é VENDEDOR ou EMPRESA
+    require_once "../navbar/navbarVendEmp.php";
+
+} else { // DESLOGADO
+    require_once "../navbar/navbarDeslogado.php";
+}
+
+$idComprador = $_SESSION["idSessao"]; // Obtém o ID do comprador logado
+
+$resultado = buscarHistorico($idComprador);
+?>
+
+
+
+<php>
 <div class="container mt-5">
     <h1 class="text-center" style="color: #502779;"><b>Histórico de Compras</b></h1>
     <table class="table table-bordered mt-4">
@@ -63,6 +86,13 @@ $resultado = buscarHistorico($idComprador);
         </tbody>
     </table>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
